dc637970440e67048253e847d09e2a9bf7881984
anitajing <anita824@gmail.com>
2026-03-05 21:18:48 -0500

    Implement service worker for caching assets

--- pattern-detective-ipad/sw.js
+++ pattern-detective-ipad/sw.js
@@ -1,1 +1,25 @@
+const CACHE = "detective-cat-v1";
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
